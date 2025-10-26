86223ddfaf7fa148fce176dd673052ca8ac8a629
singhmanmeetsingh <singhmanmeet523@gmail.com>
2025-10-26 16:14:52 -0400

    Added kafka producer

---
+++ main/backend/services/KafkaProducer.js
@@ -0,0 +1,58 @@
+import Kafka from 'kafkajs';
+
+
+const kafka = new Kafka({
+    clientId: 'movement-websocket',
+    brokers: ['localhost:9092']
+});
+
+
+const producer = kafka.producer();
+
+const isConnected = false;
+
+
+
+
+async function connectProducer() {
+    try {
+        await producer.connect();
+        console.log('Producer connected');
+    } catch (error) {
+        console.error('Error connecting producer:', error);
+    }
+}
+
+
+
+
+async function disconnectProducer() {
+    try {
+        await producer.disconnect();
+        console.log('Producer disconnected');
+    } catch (error) {
+        console.error('Error disconnecting producer:', error);
+    }
+}
+
+
+
+export async function sendMessage(topic, message) {
+    try {
+
+        if (isConnected == false) {
+            await connectProducer();
+        }
+        await producer.send({
+            topic: topic,
+            messages: [{ value: message }]
+        });
+        console.log('Message sent successfully');
+    } catch (error) {
+        console.error('Error sending message:', error);
+    } finally {
+        await disconnectProducer();
+    }
+}
+
+
